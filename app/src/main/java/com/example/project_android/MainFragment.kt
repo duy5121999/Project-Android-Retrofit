84adc5d8bb019b5f9f2025329743f148019f7fef
bakegamon136 <thailh2000vn@gmail.com>
2021-01-23 20:24:25 +0700

    fragments from our DrawerLayout

--- app/src/main/java/com/example/project_android/MainFragment.kt
+++ app/src/main/java/com/example/project_android/MainFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.project_android.Model.Product
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 class MainFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,27 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val root = inflater.inflate(R.layout.fragment_main, container, false)
+        val products = arrayListOf<Product>()
+
+        for (i in 0..100){
+            products.add(
+                Product(
+                1.0,
+                "Lamborgini #$i",
+                "http://via.placeholder.com/200x200",
+                "",
+                46.0,
+                239.0
+            )
+            )
+        }
+
+        root.recycler_view.apply {
+            layoutManager = GridLayoutManager(activity,2)
+            adapter = ProductsAdapter(products)
+        }
+
+        return root
     }
 }
